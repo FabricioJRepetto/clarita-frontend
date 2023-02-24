15cf6e7a23a24dd4edfe3cf56d335f30980004d0
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2023-02-24 16:09:25 -0300

    home shortcuts

--- src/components/home/widgets/shortcut/ShortCut.jsx
+++ src/components/home/widgets/shortcut/ShortCut.jsx
@@ -5,10 +5,10 @@ const ShortCut = ({ title, to }) => {
     const navigate = useNavigate()
 
     return (
-        <div className='w-full h-24 flex justify-center items-center cursor-pointer bg-gradient-to-br from-slate-700/30 to-transparent hover:brightness-125 rounded-lg overflow-clip'
+        <div className='w-full h-24 flex justify-center items-center cursor-pointer hover:brightness-125 rounded-lg overflow-clip'
             onClick={() => navigate(to)}>
 
-            <p className='text-2xl font-bold'>
+            <p className='text-2xl font-bold short-cut'>
                 {title}
             </p>
         </div>
